43b1fd008a6dc98cd2b5ad0e4aa84dc1a7ec7882
Aasish <aasishsrivatsa25@gmail.com>
2023-05-15 17:57:14 +0530

    Add basic structure

---
+++ FlashCard-Backend.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
+import random
+import threading
+
+class Server:
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
+    
