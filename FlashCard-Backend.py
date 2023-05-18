fbbe9e9c7605aa6c1f0d29e29f4e6730dbceed27
Aasish <aasishsrivatsa25@gmail.com>
2023-05-18 20:24:25 +0530

    add more functionality to backend

--- FlashCard-Backend.py
+++ FlashCard-Backend.py
@@ -3,23 +3,25 @@ from flask_cors import CORS
 import random
 import csv
 
-class Backend:
-    csv_file_path = 'flashcards.csv'
+csv_path = 'flashcards.csv'
 
-    def __init__(self):
+class Backend:
+
+    def __init__(self,path = csv_path) -> None:
+        self.csv_file_path = path
         self.app = Flask(__name__)
         CORS(self.app)
         self.questions = []
         self.load_flashcards()
 
-        self.app.route('/')(self.index)
+        self.app.route('/')(self.hompage)
         self.app.route('/add-card')(self.add_card)
         self.app.route('/start-app')(self.start)
         self.app.route('/flashcard', methods=['GET'])(self.get_flashcard)
         self.app.route('/save_flashcard', methods=['POST'])(self.save_flashcard)
 
-    def index(self):
-        return render_template("index.html")
+    def hompage(self):
+        return render_template("homepage.html")
     
     def add_card(self):
         return render_template("add-card.html")
@@ -45,32 +47,48 @@ class Backend:
         # Get user inputs from the request
         question = request.form.get("question")
         answer = request.form.get("answer")
-        time_taken = request.form.get("time")
-        correctness = request.form.get("correctness")
+        time_taken = 0
+        correctness = True
+        number_of_times_seen = 0
 
         # Replace commas with pipes
         question = question.replace(",", "|")
         answer = answer.replace(",", "|")
-        time_taken = time_taken.replace(",", "|")
-        correctness = correctness.replace(",", "|")
 
         if not question or not answer:
             # Return an error response indicating missing question or answer
             return jsonify({'success': False, 'error': 'Question and answer are required.'})
         else:
-            try:
-                # Save the data to the CSV file
-                with open(self.csv_file_path, 'a', newline='') as file:
-                    writer = csv.writer(file)
-                    writer.writerow([question, answer, time_taken, correctness])
-                    self.questions.append([question, answer, time_taken, correctness])
-            except Exception as e:
-                # Handle the exception, e.g., print an error message or log the exception
-                print(f"An error occurred while saving the flashcard: {e}")
-                return jsonify({"success": False})
-
+            database.append_data([question, answer, time_taken, correctness, number_of_times_seen])
             return jsonify({"success": True})
+
+
+class DataHandler:
+    def __init__(self,path=csv_path) -> None:
+        self.csv_file_path = path
+    
+    def append_data(self, data):
+        try:
+            self.question = data[0]
+            self.answer = data[1]
+            self.time_taken = data[2]
+            self.correctness = data[3]
+            self.number_of_times_seen = data[4]
+
+            # Save the data to the CSV file
+
+            with open(self.csv_file_path, 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([self.question, self.answer, self.time_taken, self.correctness, self.number_of_times_seen])
+                self.questions.append([self.question, self.answer, self.time_taken, self.correctness, self.number_of_times_seen])
+
+        except Exception as e:
+
+            # Handle the exception, e.g., print an error message or log the exception
+            print(f"An error occurred while saving the flashcard: {e}")
+            return jsonify({"success": False})
 
 if __name__ == "__main__":
     server = Backend()
+    database = DataHandler()
     server.app.run(host="0.0.0.0", debug=True)
