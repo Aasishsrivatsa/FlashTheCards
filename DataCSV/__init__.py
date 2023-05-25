4e32e70a30c40a6ffe983c16516b2cb8800a584e
Aasish <50045306+Aasishsrivatsa@users.noreply.github.com>
2023-05-25 12:21:53 +0530

    Update __init__.py

--- DataCSV/__init__.py
+++ DataCSV/__init__.py
@@ -2,9 +2,8 @@ import csv
 from flask import jsonify
 
 class DataHandler:
-
     @staticmethod
-    def update_flashcard(data:list, path:str) -> bool:
+    def update_flashcard(data: list, path: str) -> bool:
         question = data[0]
         time_taken = data[1]
         correctness = data[2]
@@ -33,9 +32,6 @@ class DataHandler:
                     writer = csv.writer(file)
                     writer.writerows(rows)
 
-                # Sort the flashcards using a separate thread
-                threading.Thread(target=DataHandler.sort_flashcards, args=(path,)).start()
-
             return updated
 
         except Exception as e:
@@ -43,14 +39,12 @@ class DataHandler:
             print(f"An error occurred while updating the flashcard: {e}")
 
             return updated
-        
-        DataHandler.sort_flashcards(path)
 
     @staticmethod
-    def read_csv(path:str) -> list:
+    def read_csv(path: str) -> list:
         questions = []
         try:
-            with open(path, 'r', encoding = 'utf-8') as file:
+            with open(path, 'r', encoding='utf-8') as file:
                 reader = csv.reader(file)
                 questions = list(reader)
         except Exception as e:
@@ -59,10 +53,8 @@ class DataHandler:
 
         return questions
 
-        DataHandler.sort_flashcards(path)
-    
     @staticmethod
-    def append_data(data:list ,path:str) -> None:
+    def append_data(data: list, path: str) -> None:
         try:
             question = data[0]
             answer = data[1]
@@ -71,21 +63,17 @@ class DataHandler:
             number_of_times_failed = data[4]
 
             # Save the data to the CSV file
-
-            with open(path, 'a', newline='', encoding = 'utf-8') as file:
+            with open(path, 'a', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
                 writer.writerow([question, answer, time_taken, correctness, number_of_times_failed])
 
         except Exception as e:
-
             # Handle the exception, e.g., print an error message or log the exception
             print(f"An error occurred while saving the flashcard: {e}")
             return jsonify({"success": False})
-        
-        DataHandler.sort_flashcards(path)
 
     @staticmethod
-    def sort_flashcards(path:str) -> None:
+    def sort_flashcards(path: str) -> None:
         try:
             questions = DataHandler.read_csv(path)
 
@@ -95,7 +83,7 @@ class DataHandler:
 
             questions.sort(key=lambda x: (x[3] is False, -float(x[2]), int(x[4])), reverse=True)
 
-            with open(path, 'w', newline='',encoding = 'utf-8') as file:
+            with open(path, 'w', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
                 writer.writerows(questions)
         except IndexError:
