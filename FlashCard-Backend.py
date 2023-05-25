600492ca0f59d7015192a9d6511db251d1f45d6b
Aasish <50045306+Aasishsrivatsa@users.noreply.github.com>
2023-05-25 13:08:55 +0530

    Update FlashCard-Backend.py to use coroutines

--- FlashCard-Backend.py
+++ FlashCard-Backend.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 import random
-import threading
 from DataCSV import DataHandler
+import asyncio
 
 csv_path = 'flashcards.csv'
 
@@ -12,6 +12,7 @@ class Backend:
         self.app = Flask(__name__)
         CORS(self.app)
         self.questions = []
+        self.data_handler = DataHandler()
         self.load_flashcards()
 
         self.app.route('/')(self.homepage)
@@ -20,29 +21,24 @@ class Backend:
         self.app.route('/flashcard', methods=['GET'])(self.get_flashcard)
         self.app.route('/save_flashcard', methods=['POST'])(self.save_flashcard)
         self.app.route('/update_flashcard', methods=['POST'])(self.update_flashcard)
-
-    def run_in_thread(self, func):
-        def wrapper(*args, **kwargs):
-            threading.Thread(target=func, args=args, kwargs=kwargs).start()
-        return wrapper
-
-    def homepage(self):
+        
+    async def homepage(self):
         return render_template("homepage.html")
 
-    def add_card(self):
+    async def add_card(self):
         return render_template("add-card.html")
 
-    def start(self):
+    async def start(self):
         return render_template("start-app.html")
 
     def load_flashcards(self):
-        self.questions = DataHandler.read_csv(self.csv_file_path)
+        self.questions = self.data_handler.read_csv(self.csv_file_path)
 
-    def get_flashcard(self):
+    async def get_flashcard(self):
         if len(self.questions) == 0:
             self.load_flashcards()
 
-        flashcard1 = self.questions[random.randint(0, 2]
+        flashcard1 = self.questions[random.randint(0, 2)]
         flashcard2 = random.choice(self.questions)
 
         flashcard_chooser = []
@@ -57,7 +53,7 @@ class Backend:
         }
         return jsonify(response)
 
-    def save_flashcard(self):
+    async def save_flashcard(self):
         question = request.form.get("question")
         answer = request.form.get("answer")
         time_taken = 0
@@ -72,20 +68,17 @@ class Backend:
         if not question or not answer:
             return jsonify({"success": False, "message": "An error occurred while saving the flashcard."})
         else:
-            append = DataHandler.append_data(question_list, self.csv_file_path)
-            self.run_in_thread(append)
+            await self.data_handler.append_data(question_list, self.csv_file_path)
             self.questions.append(question_list)
             return jsonify({"success": True, "message": "Flashcard saved!"})
 
-    def update_flashcard(self):
+    async def update_flashcard(self):
         question = request.form.get("question")
         time_taken = request.form.get("time")
         correctness = request.form.get("correctness")
         
         data = [question, time_taken, correctness]
-        update = DataHandler.update_flashcard(data, self.csv_file_path)
-        
-        self.run_in_thread(update)
+        await self.data_handler.update_flashcard(data, self.csv_file_path)
         
         return jsonify({"success": True, "message": "Response collected"})
 
